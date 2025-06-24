a64bca1686b4582d15b4c7fe16e6a7dc288bb805
Nojus Visneveckas <noshkelt@gmail.com>
2025-06-24 19:46:06 +0100

    Add project files.

---
+++ suspension-demo-opengl.cpp
@@ -0,0 +1,12 @@
+﻿// suspension-demo-opengl.cpp : Defines the entry point for the application.
+//
+
+#include "suspension-demo-opengl.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
